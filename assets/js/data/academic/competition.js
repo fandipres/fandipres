1052caeda2d48834e49fb03c1ab767fc1a03b1e2
Fandi Presly Simamora <fandi.simamora@gmail.com>
2026-07-18 14:24:10 +0700

    feat: add competition page and related JavaScript data for academic competitions

---
+++ assets/js/data/academic/competition.js
@@ -0,0 +1,22 @@
+const competition = [
+    {
+        year: "2025",
+        title: {
+            id: "Agrivision: Aplikasi Diagnosis dan Solusi Penyakit Padi Berbasis Machine Learning",
+            en: "Agrivision: Machine Learning-Based Rice Disease Diagnosis and Solution Application"
+        },
+        subtitle: {
+            id: "Kementerian Pendidikan Tinggi, Sains, dan Teknologi Republik Indonesia",
+            en: "Ministry of Higher Education, Science, and Technology of the Republic of Indonesia"
+        },
+        category: {
+            id: "Program Kreativitas Mahasiswa (PKM-KC)",
+            en: "Student Creativity Program (PKM-KC)"
+        },
+        role: { id: "Pembimbing Utama", en: "Lead Supervisor" },
+        achievement: { id: "Tidak Lolos Pendanaan", en: "Not Selected for Funding" },
+        description: { id: "", en: "" },
+        students: ["Amanda Grace Patricia Sinurat", "Astriva Novaria Hutabalian", "Ferdinanta Ginting", "Mariana Kristi Sintauli Lumbantobing", "Vio Azuhri"],
+        assets: {}
+    }
+];
